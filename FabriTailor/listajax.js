19f14cc6c47a462ca13e0b0f20d1742bf98524f7
ggkk0818 <guoyang79984>
2015-06-21 00:07:55 +0800

    完成商品详情页模板

---
+++ FabriTailor/listajax.js
@@ -0,0 +1,62 @@
+﻿{
+    "orderTypes":["topDesc","priceAsc","priceDesc","salesDesc","scoreDesc","dateDesc"],
+    "productCategory":{"id":1,"createDate":1431187291000,"modifyDate":1431187291000,"order":1},
+    "brand":null,
+    "promotion":null,
+    "tags":[],
+    "attributeValue":{},
+    "startPrice":null,
+    "endPrice":null,
+    "orderType":null,
+    "pageNumber":null,
+    "pageSize":null,
+    "page":{
+        "content":[
+            {"id":300,"createDate":1431188402000,"modifyDate":1431188402000,"sn":"201304529","name":"尚都比拉女装2013夏装新款蕾丝连衣裙 韩版修身雪纺打底裙子 春款","fullName":"尚都比拉女装2013夏装新款蕾丝连衣裙 韩版修身雪纺打底裙子 春款[白色 M]","price":298.000000,"image":"http://storage.shopxx.net/demo-image/3.0/201301/0ff130db-0a1b-4b8d-a918-ed9016317009-thumbnail.jpg","unit":"件","isGift":false,"path":"/product/content/201505/300.html","thumbnail":"http://storage.shopxx.net/demo-image/3.0/201301/0ff130db-0a1b-4b8d-a918-ed9016317009-thumbnail.jpg"},
+            {"id":298,"createDate":1431188400000,"modifyDate":1431188400000,"sn":"201304527","name":"尚都比拉2013夏装新款淑女装 春款森女系 碎花修身短袖蕾丝连衣裙","fullName":"尚都比拉2013夏装新款淑女装 春款森女系 碎花修身短袖蕾丝连衣裙[粉红色 S]","price":289.000000,"image":"http://storage.shopxx.net/demo-image/3.0/201301/f37ba36a-181b-4161-a88f-f74c9adb485d-thumbnail.jpg","unit":"件","isGift":false,"path":"/product/content/201505/298.html","thumbnail":"http://storage.shopxx.net/demo-image/3.0/201301/f37ba36a-181b-4161-a88f-f74c9adb485d-thumbnail.jpg"},
+            {"id":293,"createDate":1431188395000,"modifyDate":1431188395000,"sn":"201304522","name":"尚都比拉2013夏装新款女装 韩版淑女装 蕾丝雪纺连衣裙 春款短袖","fullName":"尚都比拉2013夏装新款女装 韩版淑女装 蕾丝雪纺连衣裙 春款短袖[白色 S]","price":259.000000,"image":"http://storage.shopxx.net/demo-image/3.0/201301/93b9e55f-cbae-4c3c-b2f7-2636c7369db7-thumbnail.jpg","unit":"件","isGift":false,"path":"/product/content/201505/293.html","thumbnail":"http://storage.shopxx.net/demo-image/3.0/201301/93b9e55f-cbae-4c3c-b2f7-2636c7369db7-thumbnail.jpg"},
+            {"id":290,"createDate":1431188392000,"modifyDate":1431188392000,"sn":"201304519","name":"尚都比拉2013夏装新款 韩版优雅甜美淑女装 春款蕾丝雪纺连衣裙子","fullName":"尚都比拉2013夏装新款 韩版优雅甜美淑女装 春款蕾丝雪纺连衣裙子[绿色 M]","price":299.000000,"image":"http://storage.shopxx.net/demo-image/3.0/201301/51afeef5-f6cb-4936-abea-315cfca0edc0-thumbnail.jpg","unit":"件","isGift":false,"path":"/product/content/201505/290.html","thumbnail":"http://storage.shopxx.net/demo-image/3.0/201301/51afeef5-f6cb-4936-abea-315cfca0edc0-thumbnail.jpg"},
+            {"id":287,"createDate":1431188389000,"modifyDate":1431188389000,"sn":"201304516","name":"尚都比拉2013夏装新款 韩版优雅淑女装 七分袖蕾丝雪纺连衣裙春款","fullName":"尚都比拉2013夏装新款 韩版优雅淑女装 七分袖蕾丝雪纺连衣裙春款[白色 S]","price":339.000000,"image":"http://storage.shopxx.net/demo-image/3.0/201301/059b5245-e3c8-43bf-80fe-700f0e4e68b8-thumbnail.jpg","unit":"件","isGift":false,"path":"/product/content/201505/287.html","thumbnail":"http://storage.shopxx.net/demo-image/3.0/201301/059b5245-e3c8-43bf-80fe-700f0e4e68b8-thumbnail.jpg"},
+            {"id":285,"createDate":1431188387000,"modifyDate":1431188387000,"sn":"201304514","name":"尚都比拉2013夏装新款 春款修身淑女装 雪纺短袖假两件套连衣裙子","fullName":"尚都比拉2013夏装新款 春款修身淑女装 雪纺短袖假两件套连衣裙子[绿色 S]","price":269.000000,"image":"http://storage.shopxx.net/demo-image/3.0/201301/4a51167a-89d5-4710-aca2-7c76edc355b8-thumbnail.jpg","unit":"件","isGift":false,"path":"/product/content/201505/285.html","thumbnail":"http://storage.shopxx.net/demo-image/3.0/201301/4a51167a-89d5-4710-aca2-7c76edc355b8-thumbnail.jpg"},
+            {"id":282,"createDate":1431188384000,"modifyDate":1431188384000,"sn":"201304511","name":"尚都比拉2013夏装新款 春款甜美淑女装 荷叶袖修身蕾丝雪纺连衣裙","fullName":"尚都比拉2013夏装新款 春款甜美淑女装 荷叶袖修身蕾丝雪纺连衣裙[白色 M]","price":269.000000,"image":"http://storage.shopxx.net/demo-image/3.0/201301/750a9ce8-8c19-444d-b8cc-f3e7e786ec5d-thumbnail.jpg","unit":"件","isGift":false,"path":"/product/content/201505/282.html","thumbnail":"http://storage.shopxx.net/demo-image/3.0/201301/750a9ce8-8c19-444d-b8cc-f3e7e786ec5d-thumbnail.jpg"},
+            {"id":279,"createDate":1431188381000,"modifyDate":1431188381000,"sn":"201304508","name":"尚都比拉2013春夏装新款女装 春款修身女裙 蕾丝雪纺短袖连衣裙子","fullName":"尚都比拉2013春夏装新款女装 春款修身女裙 蕾丝雪纺短袖连衣裙子[粉红色 S]","price":266.000000,"image":"http://storage.shopxx.net/demo-image/3.0/201301/2553e635-7aa4-416a-83f4-5288145684a1-thumbnail.jpg","unit":"件","isGift":false,"path":"/product/content/201505/279.html","thumbnail":"http://storage.shopxx.net/demo-image/3.0/201301/2553e635-7aa4-416a-83f4-5288145684a1-thumbnail.jpg"},
+            {"id":275,"createDate":1431188377000,"modifyDate":1431188377000,"sn":"201304423","name":"尚都比拉2013夏装新款女装 春款蕾丝上衣 甜美打底衫雪纺衫衬衫女","fullName":"尚都比拉2013夏装新款女装 春款蕾丝上衣 甜美打底衫雪纺衫衬衫女[白色 M]","price":199.000000,"image":"http://storage.shopxx.net/demo-image/3.0/201301/ed261f1a-72b4-47f6-b6bf-a3c70485b833-thumbnail.jpg","unit":"件","isGift":false,"path":"/product/content/201505/275.html","thumbnail":"http://storage.shopxx.net/demo-image/3.0/201301/ed261f1a-72b4-47f6-b6bf-a3c70485b833-thumbnail.jpg"},
+            {"id":271,"createDate":1431188373000,"modifyDate":1431188373000,"sn":"201304419","name":"唯维欣怡2013春夏季新款波西米亚女装中长款百褶蕾丝雪纺连衣裙子","fullName":"唯维欣怡2013春夏季新款波西米亚女装中长款百褶蕾丝雪纺连衣裙子[白色 S]","price":268.990000,"image":"http://storage.shopxx.net/demo-image/3.0/201301/1726166a-5f3b-4483-ab25-cb187fde78dd-thumbnail.jpg","unit":"件","isGift":false,"path":"/product/content/201505/271.html","thumbnail":"http://storage.shopxx.net/demo-image/3.0/201301/1726166a-5f3b-4483-ab25-cb187fde78dd-thumbnail.jpg"},
+            {"id":267,"createDate":1431188369000,"modifyDate":1431188369000,"sn":"201304415","name":"唯维欣怡2013春夏季新款韩版大码宽松显瘦女装荷叶边雪纺连衣裙子","fullName":"唯维欣怡2013春夏季新款韩版大码宽松显瘦女装荷叶边雪纺连衣裙子[黑色 S]","price":206.000000,"image":"http://storage.shopxx.net/demo-image/3.0/201301/b499fb5e-999f-431b-a375-172ee09e4a3e-thumbnail.jpg","unit":"件","isGift":false,"path":"/product/content/201505/267.html","thumbnail":"http://storage.shopxx.net/demo-image/3.0/201301/b499fb5e-999f-431b-a375-172ee09e4a3e-thumbnail.jpg"},
+            {"id":261,"createDate":1431188363000,"modifyDate":1431188363000,"sn":"201304409","name":"唯维欣怡2013春装新款韩版女装修身网纱长袖衬衣休闲女士白色衬衫","fullName":"唯维欣怡2013春装新款韩版女装修身网纱长袖衬衣休闲女士白色衬衫[白色 S]","price":199.000000,"image":"http://storage.shopxx.net/demo-image/3.0/201301/4652d7ae-3d2c-4692-89ea-0ca81f50eac3-thumbnail.jpg","unit":"件","isGift":false,"path":"/product/content/201505/261.html","thumbnail":"http://storage.shopxx.net/demo-image/3.0/201301/4652d7ae-3d2c-4692-89ea-0ca81f50eac3-thumbnail.jpg"},
+            {"id":255,"createDate":1431188357000,"modifyDate":1431188357000,"sn":"201304352","name":"维依恋 2013春秋装新款 韩版修身短款女士机车大码pu皮衣短外套","fullName":"维依恋 2013春秋装新款 韩版修身短款女士机车大码pu皮衣短外套[黑色 M]","price":266.000000,"image":"http://storage.shopxx.net/demo-image/3.0/201301/d1d15c1d-e5da-414e-ac53-745b56ccf369-thumbnail.jpg","unit":"件","isGift":false,"path":"/product/content/201505/255.html","thumbnail":"http://storage.shopxx.net/demo-image/3.0/201301/d1d15c1d-e5da-414e-ac53-745b56ccf369-thumbnail.jpg"},
+            {"id":254,"createDate":1431188356000,"modifyDate":1431188356000,"sn":"201304351","name":"维依恋2013春装新款韩版双排扣中长款女式风衣女装外套春款大衣","fullName":"维依恋2013春装新款韩版双排扣中长款女式风衣女装外套春款大衣[卡其色 M]","price":269.000000,"image":"http://localhost:8080http://storage.shopxx.net/demo-image/3.0/201301/b967015f-bc3b-47af-97d5-6446d7a590cb-thumbnail.jpg","unit":"件","isGift":false,"path":"/product/content/201505/254.html","thumbnail":"http://storage.shopxx.net/demo-image/3.0/201301/b967015f-bc3b-47af-97d5-6446d7a590cb-thumbnail.jpg"},
+            {"id":246,"createDate":1431188348000,"modifyDate":1431188348000,"sn":"201304343","name":"维依恋2013夏装新款波西米亚印花雪纺半身裙抹胸连衣裙两穿长裙子","fullName":"维依恋2013夏装新款波西米亚印花雪纺半身裙抹胸连衣裙两穿长裙子[混色 S]","price":199.000000,"image":"http://storage.shopxx.net/demo-image/3.0/201301/698a395e-ac95-4f76-a3c9-aa4e5fbc9217-thumbnail.jpg","unit":"件","isGift":false,"path":"/product/content/201505/246.html","thumbnail":"http://storage.shopxx.net/demo-image/3.0/201301/698a395e-ac95-4f76-a3c9-aa4e5fbc9217-thumbnail.jpg"},
+            {"id":239,"createDate":1431188341000,"modifyDate":1431188341000,"sn":"201304336","name":"维依恋2013夏装新款韩版修身娃娃领女式短袖雪纺衫蕾丝拼接上衣","fullName":"维依恋2013夏装新款韩版修身娃娃领女式短袖雪纺衫蕾丝拼接上衣[粉红色 S]","price":168.000000,"image":"http://storage.shopxx.net/demo-image/3.0/201301/aec6d0ae-cad6-4cca-96bb-4bcd25e994cb-thumbnail.jpg","unit":"件","isGift":false,"path":"/product/content/201505/239.html","thumbnail":"http://storage.shopxx.net/demo-image/3.0/201301/aec6d0ae-cad6-4cca-96bb-4bcd25e994cb-thumbnail.jpg"},
+            {"id":205,"createDate":1431188307000,"modifyDate":1431188307000,"sn":"201304302","name":"伊芙丽2013春款新款女士西装领一粒扣短款小西装外套女1161024-2","fullName":"伊芙丽2013春款新款女士西装领一粒扣短款小西装外套女1161024-2[粉红色 S]","price":358.000000,"image":"http://storage.shopxx.net/demo-image/3.0/201301/f5e39c37-94b2-462e-8e58-8bde3c5f1b8c-thumbnail.jpg","unit":"件","isGift":false,"path":"/product/content/201505/205.html","thumbnail":"http://storage.shopxx.net/demo-image/3.0/201301/f5e39c37-94b2-462e-8e58-8bde3c5f1b8c-thumbnail.jpg"},
+            {"id":199,"createDate":1431188301000,"modifyDate":1431188301000,"sn":"201304296","name":"伊芙丽 春款夏装新款 女装复古短袖镂空花案 蕾丝连衣裙6339774","fullName":"伊芙丽 春款夏装新款 女装复古短袖镂空花案 蕾丝连衣裙6339774[米黄色 S]","price":469.880000,"image":"http://storage.shopxx.net/demo-image/3.0/201301/f4fc0bd7-71d1-4dff-950d-6ebb8221388c-thumbnail.jpg","unit":"件","isGift":false,"path":"/product/content/201505/199.html","thumbnail":"http://storage.shopxx.net/demo-image/3.0/201301/f4fc0bd7-71d1-4dff-950d-6ebb8221388c-thumbnail.jpg"},
+            {"id":194,"createDate":1431188296000,"modifyDate":1431188296000,"sn":"201304291","name":"伊芙丽 2013春装新款 女装OL通勤修身长袖小外套女小西装1211120","fullName":"伊芙丽 2013春装新款 女装OL通勤修身长袖小外套女小西装1211120[卡其色 XS]","price":568.000000,"image":"http://storage.shopxx.net/demo-image/3.0/201301/69439b45-8626-451c-a31a-08f9df5d702b-thumbnail.jpg","unit":"件","isGift":false,"path":"/product/content/201505/194.html","thumbnail":"http://storage.shopxx.net/demo-image/3.0/201301/69439b45-8626-451c-a31a-08f9df5d702b-thumbnail.jpg"},
+            {"id":134,"createDate":1431188236000,"modifyDate":1431188236000,"sn":"201304231","name":"OSA女装雪纺衬衫 长袖韩版蕾丝休闲衬衣女C22432","fullName":"OSA女装雪纺衬衫 长袖韩版蕾丝休闲衬衣女C22432[绿色 S]","price":288.500000,"image":"http://storage.shopxx.net/demo-image/3.0/201301/1a53a089-fe86-4826-92a9-019c27463d90-thumbnail.jpg","unit":"件","isGift":false,"path":"/product/content/201505/134.html","thumbnail":"http://storage.shopxx.net/demo-image/3.0/201301/1a53a089-fe86-4826-92a9-019c27463d90-thumbnail.jpg"}
+        ],
+        "total":32,
+        "pageable":{
+            "pageNumber":1,
+            "pageSize":20,
+            "searchProperty":null,
+            "searchValue":null,
+            "orderProperty":null,
+            "orderDirection":null,
+            "filters":[],
+            "orders":[
+                {"property":"isTop","direction":"desc"},
+                {"property":"modifyDate","direction":"desc"}
+            ]
+        },
+        "searchValue":null,
+        "orderDirection":null,
+        "orderProperty":null,
+        "searchProperty":null,
+        "pageSize":20,
+        "pageNumber":1,
+        "filters":[],
+        "orders":[
+            {"property":"isTop","direction":"desc"},
+            {"property":"modifyDate","direction":"desc"}
+        ],
+        "totalPages":2
+}}
